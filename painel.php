b7c0401b7c4ad0ba22d2b8dc745b67f89ed66f71
gabrielvarela707-hub <294264550+gabrielvarela707-hub@users.noreply.github.com>
2026-09-23 14:41:10 -0300

    Update painel.php

--- painel.php
+++ painel.php
@@ -155,7 +155,7 @@ $result_avaliacoes = $conn->query($sql_avaliacoes);
         <main class="crm-main">
             <!-- CABEÇALHO -->
             <div class="crm-header">
-                <h2 style="margin: 0; color: #38bdf8;">Painel de Controle /h2>
+                <h2 style="margin: 0; color: #38bdf8;">Painel de Controle </h2>
                 <span style="color: #94a3b8; font-weight: bold;">Sessão Ativa</span>
             </div>
 
